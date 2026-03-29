9927d17f87e4f16cc19f183900761e815b73941a
laxmikant7114 <laxmikant7114@users.noreply.github.com>
2026-03-29 12:53:02 +0000

    deploy: 02d1d7286cbe6ac4ee887296b2bd701d39660c06

--- _next/static/hWGNr4FjxPWiHE7IBEg5e/_buildManifest.js
+++ _next/static/hWGNr4FjxPWiHE7IBEg5e/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/My-Portfolio-Site//_next/:path+",
+        "destination": "/My-Portfolio-Site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
